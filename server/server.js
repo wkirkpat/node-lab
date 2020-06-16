7b784138c3dde3ca649f22aeff216cfa869dc8e5
Will <wk1394@gmail.com>
2020-06-16 14:26:35 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+let path = require("path");
+let fs = require("fs");
+
+let chirpArr = [
+    {
+        Username: "l33tGamerBoixXx",
+        Comment: "There"
+    },
+    {
+        Username: "oodlesofnoodles",
+        Comment: "Is"
+    },
+    {
+        Username: "pandanarchy",
+        Comment: "A"
+    },
+    {
+        Username: "johngoodmanslefteyeball",
+        Comment: "Bird"
+    },
+    {
+        Username: "yourlegosareworthless",
+        Comment: "Somewhere"
+    }
+]
+
